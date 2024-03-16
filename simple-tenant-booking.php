fd334d3876ee4f5c8da8645dfc64b6a6ce7005e6
thomasfinnegan14 <finneganthomas5@gmail.com>
2024-03-16 15:09:14 -0400

    Add plugin header

---
+++ simple-tenant-booking.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Plugin Name:       Simple Tenant Booking
+ * Plugin URI:        https://simpletenantbooking.tfwsolutions.com/
+ * Description:       A simple booking plugin for multi-tenant property owners.
+ * Version:           1.0.0
+ * Requires at least: 6.3
+ * Requires PHP:      7.4
+ * Author:            TF Web Solutions
+ * Author URI:        https://tfwsolutions.com/
+ * License:           GPL v3
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.en.html
+ * Update URI:        https://simpletenantbooking.tfwsolutions.com/
+ */
+?>
