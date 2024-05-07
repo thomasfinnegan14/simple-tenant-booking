2284c79060c3826216966aae07514922eb79a9fe
thomasfinnegan14 <finneganthomas5@gmail.com>
2024-05-07 16:30:03 -0400

    Add booking form and optimize js

--- js/time-slots.js
+++ js/time-slots.js
@@ -5,128 +5,56 @@ jQuery(document).ready(function($) {
         var month = $(this).data('month');
         var year = $(this).data('year');
 
+        // Clear the previous slots and form
+        $('#time-slots').empty();
+        $('#booking-form').remove();
+
+        // Generate time slots
+        generateTimeSlots(day);
+
+        // Add click event to time slot buttons
+        $(document).on('click', '.time-slot-button', function() {
+            var selectedTime = $(this).text();
+            // Display the booking form with the selected time slot
+            showBookingForm(selectedTime);
+        });
+    });
+
+    function generateTimeSlots(day) {
         let today = new Date().getDate();
         let currentHour = new Date().getHours();
-
         let AMPM = 'AM';
         let nextAMPM = 'AM';
 
-        // Clear the previous slots
-        $('#time-slots').empty();
+        for (var hour = 9; hour < 17; hour++) {
+            let printHour = hour > 12 ? hour - 12 : hour;
+            let nextHour = hour + 1 > 12 ? hour - 11 : hour + 1;
+            AMPM = hour >= 12 && hour < 24 ? 'PM' : 'AM';
+            nextAMPM = (hour + 1) >= 12 && (hour + 1) < 24 ? 'PM' : 'AM';
 
-        if (day == today) {
-            for (var hour = 9; hour < 17; hour++) {
-                if (hour < 12) {
-                    AMPM = 'AM';
-                }
-                else {
-                    AMPM = 'PM';
-                }
-
-                if ((hour + 1) < 12) {
-                    nextAMPM = 'AM';
-                }
-                else {
-                    nextAMPM = 'PM';
-                }
-
-                let printHour = hour;
-                let nextHour = hour + 1;
-
-                switch (hour) {
-                    case 12:
-                        nextHour = 1;
-                        break;
-                    case 13:
-                        printHour = 1;
-                        nextHour = 2;
-                        break;
-                    case 14:
-                        printHour = 2;
-                        nextHour = 3;
-                        break;
-                    case 15:
-                        printHour = 3;
-                        nextHour = 4;
-                        break;
-                    case 16:
-                        printHour = 4;
-                        nextHour = 5;
-                        break;
-                }
-
-                if (hour <= currentHour) {
-                    var slot = $('<button>')
-                    .text(printHour + ':00 ' + AMPM + ' - ' + nextHour + ':00 ' + nextAMPM)
-                    .addClass('time-slot-button')
-                    .addClass('unavailable')
-                    .prop("disabled", true)
-                    .off('mouseenter mouseleave');
-    
-                    $('#time-slots').append(slot);
-                } else {
-                    var slot = $('<button>')
-                    .text(printHour + ':00 ' + AMPM + ' - ' + nextHour + ':00 ' + nextAMPM)
-                    .addClass('time-slot-button')
-                    .on('click', function() {
-                        // Handle booking logic here
-                        alert('Booking for ' + $(this).text());
-                    });
-    
-                    $('#time-slots').append(slot);
-                }
-            }
-        } else {
-            for (var hour = 9; hour < 17; hour++) {
-                if (hour < 12) {
-                    AMPM = 'AM';
-                }
-                else {
-                    AMPM = 'PM';
-                }
-
-                if ((hour + 1) < 12) {
-                    nextAMPM = 'AM';
-                }
-                else {
-                    nextAMPM = 'PM';
-                }
-
-                let printHour = hour;
-                let nextHour = hour + 1;
-
-                switch (hour) {
-                    case 12:
-                        nextHour = 1;
-                        break;
-                    case 13:
-                        printHour = 1;
-                        nextHour = 2;
-                        break;
-                    case 14:
-                        printHour = 2;
-                        nextHour = 3;
-                        break;
-                    case 15:
-                        printHour = 3;
-                        nextHour = 4;
-                        break;
-                    case 16:
-                        printHour = 4;
-                        nextHour = 5;
-                        break;
-                }
-
-                var slot = $('<button>')
+            var slot = $('<button>')
                 .text(printHour + ':00 ' + AMPM + ' - ' + nextHour + ':00 ' + nextAMPM)
-                .addClass('time-slot-button')
-                .on('click', function() {
-                    // Handle booking logic here
-                    alert('Booking for ' + $(this).text());
-                });
-
-                $('#time-slots').append(slot);
+                .addClass('time-slot-button');
+            
+            if (day == today && hour <= currentHour) {
+                slot.addClass('unavailable').prop("disabled", true);
             }
+
+            $('#time-slots').append(slot);
         }
-    });
+    }
+
+    function showBookingForm(selectedTime) {
+        // Remove existing form if any
+        $('#booking-form').remove();
+
+        var formHtml = '<div id="booking-form">' +
+                        '<h3>Selected time slot to reserve:</h3>' +
+                        '<p>' + selectedTime + '</p>' +
+                        '<input type="text" id="name" placeholder="Enter your name/company name" />' +
+                        '<button type="submit">Reserve</button>' +
+                       '</div>';
+        
+        $('#time-slots').after(formHtml);
+    }
 });
